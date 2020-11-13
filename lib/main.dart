b19733201cf3cb67728a3eafcca2e2801ec3367c
FikryAS <azizfikry15@gmail.com>
2020-11-13 07:08:37 +0700

    add localizations delegates

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_profilpage_cupertino_inisiated/profile.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
+      localizationsDelegates: [
+        DefaultMaterialLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
       home: ProfilePage(),
     );
   }
