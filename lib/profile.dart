99639859b73cdac30555dedbf6ee5065b1b9ee5f
FikryAS <azizfikry15@gmail.com>
2020-11-13 07:09:07 +0700

    add ternary conditional for variable null

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,9 +5,9 @@ import 'package:flutter_profilpage_cupertino_inisiated/custom/custom_button.dart
 import 'package:flutter_profilpage_cupertino_inisiated/edit_profile.dart';
 
 class ProfilePage extends StatefulWidget {
-  final String name = "Your Name";
+  final String id, name, desc, photoUrl;
 
-  ProfilePage({@required this.name});
+  ProfilePage({this.name, this.id, this.desc, this.photoUrl});
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
@@ -16,128 +16,129 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading: GestureDetector(
-          child: Icon(
-            Icons.add,
-            color: Colors.black,
+        navigationBar: CupertinoNavigationBar(
+          leading: GestureDetector(
+            child: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            onTap: () {},
           ),
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => EditProfile()));
-          },
+          middle: Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Row(
+              children: [Text(widget.id == null ? "Your Id" : widget.id)],
+            ),
+          ),
+          trailing: GestureDetector(
+            child: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onTap: () {},
+          ),
         ),
-        middle: Container(
-          margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-          child: Row(
+        child: SafeArea(
+          child: Column(
             children: [
-              Text("Your ID"),
-            ],
-          ),
-        ),
-        trailing: GestureDetector(
-          child: Icon(
-            Icons.menu,
-            color: Colors.black,
-          ),
-          onTap: () {},
-        ),
-      ),
-      child: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  border: Border(
-                      top: BorderSide(color: Colors.grey[300]),
-                      bottom: BorderSide(color: Colors.grey[300]))),
-              child: Center(
-                child: CupertinoButton(
-                  child: Text(
-                    "See Covid-19 Business Resources",
-                    style: TextStyle(fontSize: 12),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(color: Colors.grey[300]),
+                        bottom: BorderSide(color: Colors.grey[300]))),
+                child: Center(
+                  child: CupertinoButton(
+                    child: Text(
+                      "See Covid-19 Business Resources",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.grey,
-                    backgroundImage: NetworkImage(
-                        "https://www.mdblakehurst.catholic.edu.au/wp-content/uploads/sites/39/2019/05/Person-icon.jpg"),
-                  ),
-                  CustomText(numbers: 0, text: "Posts"),
-                  CustomText(numbers: 124, text: "Followers"),
-                  CustomText(numbers: 166, text: "Following"),
-                ],
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                        radius: 40,
+                        backgroundColor: Colors.grey,
+                        backgroundImage: widget.photoUrl == null
+                            ? NetworkImage(
+                                "https://www.mdblakehurst.catholic.edu.au/wp-content/uploads/sites/39/2019/05/Person-icon.jpg")
+                            : NetworkImage(widget.photoUrl)),
+                    CustomText(numbers: 0, text: "Posts"),
+                    CustomText(numbers: 124, text: "Followers"),
+                    CustomText(numbers: 166, text: "Following"),
+                  ],
+                ),
               ),
-            ),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                    margin: EdgeInsets.fromLTRB(20, 0, 0, 15),
-                    child: Text(
-                      widget.name,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                    ))),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                    margin: EdgeInsets.fromLTRB(20, 0, 0, 20),
-                    child: Text(
-                      "Your Description",
-                      style: TextStyle(fontSize: 12),
-                    ))),
-            Container(
-              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomButton(
-                      text: "Edit Profile",
-                      onProcessed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => EditProfile()));
-                      }),
-                  CustomButton(
-                      text: "Promotions",
-                      onProcessed: () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                CupertinoAlertDialog(
-                                  title: Text('Profile Changed'),
-                                  content:
-                                      Text("Do you want to save your edits"),
-                                  actions: [
-                                    CupertinoDialogAction(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: Text('Cancel')),
-                                    CupertinoDialogAction(
-                                        onPressed: () {}, child: Text('Save'))
-                                  ],
-                                ),
-                            barrierColor: Colors.black.withOpacity(0.8),
-                            barrierDismissible: false);
-                      }),
-                  CustomButton(text: "Insights", onProcessed: () {}),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      margin: EdgeInsets.fromLTRB(20, 0, 0, 15),
+                      child: Text(
+                        widget.name == null ? "Your Name" : widget.name,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 14),
+                      ))),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      margin: EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Text(
+                        widget.desc == null ? "Your Description" : widget.desc,
+                        style: TextStyle(fontSize: 14),
+                      ))),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomButton(
+                        text: "Edit Profile",
+                        onProcessed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => EditProfile(
+                                      name: widget.name,
+                                      id: widget.id,
+                                      desc: widget.desc,
+                                      photoUrl: widget.photoUrl)));
+                        }),
+                    CustomButton(
+                        text: "Promotions",
+                        onProcessed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  CupertinoAlertDialog(
+                                    title: Text('Profile Changed'),
+                                    content:
+                                        Text("Do you want to save your edits"),
+                                    actions: [
+                                      CupertinoDialogAction(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text('Cancel')),
+                                      CupertinoDialogAction(
+                                          onPressed: () {}, child: Text('Save'))
+                                    ],
+                                  ),
+                              barrierColor: Colors.black.withOpacity(0.8),
+                              barrierDismissible: false);
+                        }),
+                    CustomButton(text: "Insights", onProcessed: () {}),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+        // : Center(child: CircularProgressIndicator()),
+        );
   }
 }
